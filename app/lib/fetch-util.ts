f70917073d3f7c3885243dea7192179d6bae7288
Jedrzej <jedrzej.borakiewicz@webcrafters.pl>
2025-10-27 00:34:32 +0100

    fix connect with backend

--- app/lib/fetch-util.ts
+++ app/lib/fetch-util.ts
@@ -5,7 +5,7 @@ const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000/api-v1";
 const api = axios.create({
   baseURL: BASE_URL,
   headers: {
-    "Contetn-Type": "application/json",
+    "Content-Type": "application/json",
   },
 });
 
